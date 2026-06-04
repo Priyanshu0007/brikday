bc81f6afb5747f6f8fa5cbfa2d6eca9fe695435d
Priyanshu0007 <priyanshu2859@gmail.com>
2026-06-04 21:16:55 +0530

    fix: adjust splash screen icon width and improve animated icon positioning and scaling

--- src/components/animated-icon.tsx
+++ src/components/animated-icon.tsx
@@ -89,7 +89,7 @@ export function AnimatedIcon() {
 
       <Animated.View entering={keyframe.duration(DURATION)} style={styles.background} />
       <Animated.View style={styles.imageContainer} entering={logoKeyframe.duration(DURATION)}>
-        <Image style={styles.image} source={require('@/assets/images/expo-logo.png')} />
+        <Image style={styles.image} contentPosition='center' contentFit='contain' source={require('@/assets/images/expo-logo.png')} />
       </Animated.View>
     </View>
   );
@@ -116,6 +116,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: 76,
     height: 76,
+    marginLeft: 15
   },
   background: {
     borderRadius: 40,
